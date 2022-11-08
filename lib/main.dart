e28e7fd4bcbd588d68c097502932da3a185afc82
Golam Sarwar Shakil <gsarwarshakil@gmail.com>
2022-11-08 19:47:29 +0600

    added firebase push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:clean_api/clean_api.dart';
 import 'package:easy_localization/easy_localization.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:overlay_support/overlay_support.dart';
@@ -9,26 +11,125 @@ import 'package:zcart_seller/application/core/config.dart';
 import 'package:zcart_seller/infrastructure/app/constants.dart';
 import 'package:zcart_seller/presentation/auth/sign_in_page.dart';
 
-// import 'firebase_options.dart';
+Future<void> _firebaseMessegingBackgroundHandler(RemoteMessage message) async {
+  Logger.i('Handling a background message ${message.messageId}');
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await EasyLocalization.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance.getInitialMessage();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessegingBackgroundHandler);
+
   CleanApi.instance.setup(baseUrl: '$apiEndpoint/', showLogs: true);
-  runApp(ProviderScope(
+
+  runApp(
+    ProviderScope(
       child: EasyLocalization(
-          path: 'assets/translations',
-          supportedLocales: const [Locale('en'), Locale('bn')],
-          fallbackLocale: const Locale('en'),
-          // assetLoader: codegen,
-          child: const MyApp())));
+        path: 'assets/translations',
+        supportedLocales: const [Locale('en'), Locale('bn')],
+        fallbackLocale: const Locale('en'),
+        // assetLoader: codegen,
+        child: const MyApp(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    requestPermission();
+    initInfo();
+    getToken();
+
+    FirebaseMessaging.onMessage.listen(
+      (RemoteMessage message) async {
+        Logger.i('messege: ${message.messageId}');
+
+        BigTextStyleInformation bigTextStyleInformation =
+            BigTextStyleInformation(
+          message.notification!.body.toString(),
+          htmlFormatBigText: true,
+          contentTitle: message.notification!.title.toString(),
+          htmlFormatContentTitle: true,
+        );
+
+        AndroidNotificationDetails androidPlatformChannelSpecifics =
+            AndroidNotificationDetails(
+          'zcart',
+          'zcart',
+          importance: Importance.high,
+          styleInformation: bigTextStyleInformation,
+          priority: Priority.high,
+          playSound: true,
+        );
+
+        NotificationDetails platformChannelSpecifics =
+            NotificationDetails(android: androidPlatformChannelSpecifics);
+        await flutterLocalNotificationsPlugin.show(
+          0,
+          message.notification!.title,
+          message.notification!.body,
+          platformChannelSpecifics,
+          payload: message.data['title'],
+        );
+      },
+    );
+
+    super.initState();
+  }
+
+  void requestPermission() async {
+    FirebaseMessaging messeging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messeging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      Logger.i('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      Logger.i('User granted provisional permission');
+    } else {
+      Logger.i('User declined or has not accepted permission');
+    }
+  }
+
+  void getToken() async {
+    await FirebaseMessaging.instance
+        .getToken()
+        .then((value) => Logger.i(value));
+  }
+
+  void initInfo() {
+    var androidInitialize =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    // var iOSInitialize = const IOSini();
+    var initializationsSettings =
+        InitializationSettings(android: androidInitialize);
+    flutterLocalNotificationsPlugin.initialize(initializationsSettings,
+        onSelectNotification: ((payload) {}));
+  }
 
   @override
   Widget build(BuildContext context) {
